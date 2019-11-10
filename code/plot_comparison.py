cab1c192c197cfe13e148b2f6f4839b8083944c5
Kate Storey-Fisher <kstoreyfisher@gmail.com>
2019-11-10 18:22:34 -0500

    updated plotting code, added notebooks

--- code/plot_comparison.py
+++ code/plot_comparison.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 import plotter
-
+import utils
 
 
 plot_dir = '../plots/plots_2019-10-04'
@@ -12,54 +12,69 @@ label_dict = {'generalr': 'cosmo deriv', 'tophat': 'tophat', 'piecewise':'triang
 color_dict = {'tophat':'blue', 'standard': 'orange', 'piecewise':'crimson', 'linear_spline':'red', 'cosmo deriv':'purple', 'triangle':'crimson', 'quadratic_spline':'green', 'quadratic_spline_nbins8':'limegreen', 'quadratic':'green'}
 
 def main():
-    plot_lognormal_multi()
-
-
-def plot_lognormal_multi():
     boxsize = 750
-    nbar_str = '3e-4'
-    #nbar_str = '3e-4'
-    projs = ['quadratic_spline']
-    proj_tags = ['quadratic']
-    plot_tag = '_quadratic_nreals5'
-    nrealizations = 5
-    seeds = np.arange(nrealizations)
+    nbar_str = '1e-5'
     cat_tag = '_L{}_nbar{}'.format(boxsize, nbar_str)
+    plot_tag = '_gaussian_kernel'
+
+    #proj_tags = ['quadratic']
+    proj_tags = ['gaussian_kernel'] 
+    plot_lognormal_multi(tag, cat_tag, proj_types, plot_tag=plot_tag)
+
+
+def plot_lognormal_multi(tag, cat_tag, proj_tags, plot_tag=None, nrealizations=1, standard=False):
+    #nbar_str = '3e-4'
+    seeds = np.arange(nrealizations)
     cat_dir = '../catalogs/cats_lognormal{}'.format(cat_tag)        
     result_dir = '../results/results_lognormal{}'.format(cat_tag)
+    alpha_transparent = 0.15
+    
+    if 'py2' in cat_tag:
+        allow_pickle = False
+    else:
+        allow_pickle = True
 
     labels = []
     colors = []
+    alphas = []
 
-    rs_stan_lin = []
-    cfs_stan_lin = []
-    for seed in seeds:
-        r_stan_lin, xi_stan_lin, label_stan = np.load('{}/cf_lin_{}{}_seed{}.npy'.format(result_dir, 'standard', cat_tag, seed))
-        rs_stan_lin.append(r_stan_lin)
-        cfs_stan_lin.append(xi_stan_lin)
-        labels.append(None)
-        colors.append(color_dict['standard'])
+    rs_lin = []
+    cfs_lin = []
+    if standard:
+        rs_stan_lin = []
+        cfs_stan_lin = []
+        for seed in seeds:
+            r_stan_lin, xi_stan_lin, label_stan = np.load('{}/cf_lin_{}{}_seed{}.npy'.format(result_dir, 'standard', cat_tag, seed), allow_pickle=allow_pickle)
+            rs_stan_lin.append(r_stan_lin)
+            cfs_stan_lin.append(xi_stan_lin)
+            labels.append(None)
+            colors.append(utils.get_color('standard'))
+            alphas.append(alpha_transparent)
 
-    r_stan_mean = np.mean(rs_stan_lin, axis=0)
-    cf_stan_mean = np.mean(cfs_stan_lin, axis=0)
+        r_stan_mean = np.mean(rs_stan_lin, axis=0)
+        cf_stan_mean = np.mean(cfs_stan_lin, axis=0)
 
-    rs_lin = rs_stan_lin
-    rs_lin.append(r_stan_mean)
-    cfs_lin = cfs_stan_lin
-    cfs_lin.append(cf_stan_mean)
-    labels.append('standard')
-    colors.append(color_dict['standard'])
+        rs_lin += rs_stan_lin
+        rs_lin.append(r_stan_mean)
+        cfs_lin += cfs_stan_lin
+        cfs_lin.append(cf_stan_mean)
+        labels.append('standard')
+        colors.append(utils.get_color('standard'))
+        alphas.append(1)
+
+        
 
     for proj_tag in proj_tags:
         rs_proj_lin = []
         cfs_proj_lin = []
         for seed in seeds:
-            r_lin, xi_proj_lin, projt = np.load('{}/cf_lin_{}{}_seed{}.npy'.format(result_dir, proj_tag, cat_tag, seed))
+            r_lin, xi_proj_lin, projt = np.load('{}/cf_lin_{}{}_seed{}.npy'.format(result_dir, proj_tag, cat_tag, seed), allow_pickle=allow_pickle)
             rs_proj_lin.append(r_lin)
             cfs_proj_lin.append(xi_proj_lin)
             labels.append(None)
-            colors.append(color_dict[proj_tag])
-        
+            colors.append(utils.get_color(proj_tag))
+            alphas.append(alpha_transparent)
+
         rs_lin += rs_proj_lin
         cfs_lin += cfs_proj_lin
 
@@ -67,17 +82,18 @@ def plot_lognormal_multi():
         cf_proj_mean = np.mean(cfs_proj_lin, axis=0)
         rs_lin.append(r_proj_mean)
         cfs_lin.append(cf_proj_mean)
-        labels.append(label_dict[proj_tag])
-        colors.append(color_dict[proj_tag])
+        labels.append(utils.get_label(proj_tag))
+        colors.append(utils.get_color(proj_tag))
+        alphas.append(1)
 
     #rs_lin = rs_stan_lin + rs_proj_lin
     #cfs_lin = cfs_stan_lin + cfs_proj_lin
     
-    r_true_lin, xi_true_lin, label_true = np.load('{}/cf_lin_{}{}.npy'.format(cat_dir, 'true', cat_tag))
+    r_true_lin, xi_true_lin, label_true = np.load('{}/cf_lin_{}{}.npy'.format(cat_dir, 'true', cat_tag), allow_pickle=allow_pickle)
    
     save_lin = '{}/cf_lin{}{}.png'.format(plot_dir, cat_tag, plot_tag)
-    plotter.plot_cf_cont(rs_lin, cfs_lin, r_true_lin, xi_true_lin, labels, colors, saveto=save_lin, log=False, err=True)
-
+    ax = plotter.plot_cf_cont(rs_lin, cfs_lin, r_true_lin, xi_true_lin, labels, colors, alphas=alphas, saveto=save_lin, log=False, err=True)
+    return ax
 
 def plot_lognormal():
     
